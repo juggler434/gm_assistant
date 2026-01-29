a9ffa36548a8ad3e2b77cb42a3116f6533d4e7d5
Bill Gallagher <wgallagher86@gmail.com>
2026-01-29 13:49:00 -0800

    chore: adds test framework

---
+++ server/tests/factories/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Test factories for creating test data
+ *
+ * Factories provide consistent, customizable test data creation.
+ * Each factory returns valid default values that can be overridden.
+ */
+
+export * from "./base.js";
